efc309972d1a6e2d9b2f7e8c6352dc197c9ae2b2
amatute19 <amadomatute19@gmail.com>
2024-03-02 09:53:02 -0600

    Update home.js
    Cambio en saludo

--- src/PAGES/home.js
+++ src/PAGES/home.js
@@ -16,7 +16,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                !Hola a todos!{" "}
+                !Hola bienvenidos!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
