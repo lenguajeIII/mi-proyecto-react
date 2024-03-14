56587776490bbaff0be64fd460835630fb1f31a2
amatute19 <amadomatute19@gmail.com>
2024-03-13 20:05:51 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./PAGES/home";
+import Contactme from "./PAGES/Contactme"
 
 // import Home from "./components/Home/Home";
 // import About from "./components/About/About";
@@ -20,6 +21,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./STYLES/login.css";
 import "./STYLES/footer.css"
+import "./STYLES/Product.css";
 import "./STYLES/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./PAGES/Login";
@@ -48,6 +50,7 @@ function App() {
           <Route path="/" element={<Home />} />  
           <Route path="/galeria" element={<Galerias />} />          
           <Route path="/login" element={<Login />} />
+          <Route path="/contactme" element={<Contactme />} />
           <Route path="/food" element={<Food />} />
           <Route path="/portrait" element={<Portrait />} />
           <Route path="/product" element={<Product />} />
