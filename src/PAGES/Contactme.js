6cee4174baaaa5a29d23ab5f95045d2feccf9d2c
VanellyOlmedo <vanellyolmedo2019@gmail.com>
2024-04-08 15:20:34 -0600

    Contactme.js
    Agregue API

--- src/PAGES/Contactme.js
+++ src/PAGES/Contactme.js
@@ -11,38 +11,39 @@ function Contactme() {
 
       <html lang="es">
         <body class="flp">
-          <h1 class="h1txt">¡Contáctanos!
-
+          <h2 class="h2txt">Puedes contactar al Fotógrafo
           <p>
-          Puedes Registrarte
+          para contratar sus servicios ó
           <p>
-          para agregar imágenes
+          hacer tus preguntas
           </p>
           </p>
-          
-          </h1>
+          </h2>
           <div class="background_ctm">
             <div class="container_ctm">
-              <h2>Registro</h2>
-              <form action="#" method="post">
+              <h2>¡Contáctanos!</h2>
+              <form action="https://formsubmit.co/vanellyolmedo2019@gmail.com" method="POST">
                 <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" requid></input>
-                <label for="apellido">Apellido:</label>
-                <input type="text" id="apellido" name="apellido" required></input>
+                <input type="text" id="nombre" name="nombre" placeholder="Nombre"requid></input>
+               <label for="telefono">Teléfono:</label>
+                <input type="number" id="telefono" name="telefono" inputMode="numeric" placeholder="Teléfono"required></input>
+                <label for="edad">Edad:</label>
+                <input type="number" id="edad" name="edad" placeholder="Edad"required></input>
                 <label for="email">Correo Electrónico:</label>
                 <input type="email" id="email" name="email" placeholder="ejemplo@gmail.com" required></input>
-                <label for="telefono">Teléfono:</label>
-                <input type="number" id="telefono" name="telefono" inputMode="numeric" required></input>
-                <label for="edad">Edad:</label>
-                <input type="number" id="edad" name="edad" required></input>
-                <input type="submit" value="Registrarse"></input>
+                <label for="message">Mensaje:</label>
+                <textarea id="message" name="message" cols="30" row="5" placeholder="Mensaje" required></textarea>
+                <div class="form-txt">
+                  <a href="#">Politica de Seguridad</a>
+                  <a href="#">---</a>
+                  <a href="#">Terminos y Condiciones</a>
+                </div>
+                <input type="submit" value="Enviar"></input>
               </form>
             </div>
           </div>
         </body>
       </html>
-
-
     </Container>  
   );
 }
