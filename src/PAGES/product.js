36874c2d1fb91bb56558dab684a9da0af17a8b2b
OMAR RODRIGUEZ <98731464+David7311@users.noreply.github.com>
2024-04-23 23:13:38 -0600

    Producto
    Se modifico el codigo para agregarle lo de busqueda por fecha y por validacion de estrellas

--- src/PAGES/product.js
+++ src/PAGES/product.js
@@ -1,11 +1,12 @@
+//Librerías
 import React, { useState } from "react";
 import { Container, Row, Col, InputGroup, FormControl, Button } from "react-bootstrap";
-import Particle from "../components/Particle";
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import {AiFillInstagram, AiOutlineWhatsApp, AiFillTwitterCircle} from "react-icons/ai";
-
 import StarRatingComponent from 'react-star-rating-component';
 
+//Imagenes
 // ... Tus importaciones de imágenes ...
 import vtchileno from"../MEDIA/images/Productos/vasos/vasotqchile.jpeg";
 import ttcopa from"../MEDIA/images/Productos/vasos/ttcopa.jpeg";
@@ -16,38 +17,49 @@ import airpords from"../MEDIA/images/Productos/tecnologia/aipords.jpeg";
 import paga from"../MEDIA/images/Productos/perfumes/paga.jpeg";
 import aco from "../MEDIA/images/Productos/aretes/aritcasaoro.jpeg";
 
+// Get the URL of the current page dynamically
+//const shareUrl = typeof window !== 'undefined' ? window.location.href : '';
+// Get the Title of the current page dynamically
+//const title = typeof document !== 'undefined' ? document.title : 'Check out this awesome content!';
+
 const mockProducts = [
   
-  { id: 3,title: "Vasos Tequileros Chile", category: "Categoría A", imageUrl: vtchileno,rating: 0 },
-  { id: 4,title: "Vasos Tequileros tipo copa", category: "Categoría A", imageUrl: ttcopa,rating: 0 },
-  { id: 5,title: "Planta decorativa", category: "Categoría B", imageUrl: pdecorativa,rating: 0 },
-  { id: 6,title: "Servilletero de Navidad", category: "Categoría C", imageUrl: snavidad,rating: 0 },
-  { id: 7,title: "Servilletero Home", category: "Categoría C", imageUrl: shome,rating: 0 },
-  { id: 8,title: "Apple Airpords", category: "Categoría D", imageUrl: airpords,rating: 0 },
-  { id: 9,title: "Perfume Agua Gio Armany", category: "Categoría E", imageUrl: paga,rating: 0 },
-  { id: 10,title: "Aretes casa de oro", category: "Categoría E", imageUrl: aco,rating: 0 },
+  { id: 3,title: "Vasos Tequileros Chile", category: "Categoría A", imageUrl: vtchileno,rating: 0, date: new Date("01/01/2024").toLocaleDateString()},
+  { id: 4,title: "Vasos Tequileros tipo copa", category: "Categoría A", imageUrl: ttcopa,rating: 0,date: new Date("01/02/2024").toLocaleDateString()},
+  { id: 5,title: "Planta decorativa", category: "Categoría B", imageUrl: pdecorativa,rating: 0,date: new Date("01/03/2024").toLocaleDateString()}, 
+  { id: 6,title: "Servilletero de Navidad", category: "Categoría C", imageUrl: snavidad,rating: 0,date: new Date("01/04/2024").toLocaleDateString()},
+  { id: 7,title: "Servilletero Home", category: "Categoría C", imageUrl: shome,rating: 0,date: new Date("01/05/2024").toLocaleDateString()}, 
+  { id: 8,title: "Apple Airpords", category: "Categoría D", imageUrl: airpords,rating: 0,date: new Date("01/06/2024").toLocaleDateString()}, 
+  { id: 9,title: "Perfume Agua Gio Armany", category: "Categoría E", imageUrl: paga,rating: 0,date: new Date("01/07/2024").toLocaleDateString()},
+  { id: 10,title: "Aretes casa de oro", category: "Categoría E", imageUrl: aco,rating: 0,date: new Date("01/08/2024").toLocaleDateString()},
 
 ];
 
 
 
-
 function Product() {
-  const [products,setProducts] = useState(mockProducts);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [products, setProducts] = useState(mockProducts);
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState(''); // Estado para la categoría seleccionada
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedStar, setSelectedStar] = useState('');
+  const [showDetails, setShowDetails] = useState(Array(mockProducts.length).fill(false));
+
+  
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const handleCategoryChange = (event) => {
-    // Actualizar el estado con la categoría seleccionada
     setSelectedCategory(event.target.value);
   };
 
-  // starRating
+  const handleStarChange = (event) => {
+    setSelectedStar(event.target.value);
+  };
 
-  const onStarClick = (nextValue, prevValue, productId) => {
+  const onStarClick = (nextValue, _, productId) => {
     const updatedProducts = products.map(product => {
       if (product.id === productId) {
         return { ...product, rating: nextValue };
@@ -57,19 +69,39 @@ function Product() {
     setProducts(updatedProducts);
   };
 
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
-  // Filtrar productos por término de búsqueda y categoría seleccionada
+  const handleReset = () => {
+    setSelectedDate(null);
+    setSelectedCategory('');
+    setSelectedStar('');
+    setSearchTerm('');
+  };
+
+  const handleShowDetails = (productId) => {
+    const updatedShowDetails = [...showDetails];
+    updatedShowDetails[productId - 1] = !updatedShowDetails[productId - 1];
+    setShowDetails(updatedShowDetails);
+  };
+
   const filteredProducts = products.filter(product =>
     product.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
-    (selectedCategory ? product.category === selectedCategory : true)
+    (selectedCategory ? product.category === selectedCategory : true) &&
+    (selectedStar ? product.rating === parseInt(selectedStar) : true) &&
+    (selectedDate ? new Date(product.date).toDateString() === selectedDate.toDateString() : true)
   );
+
+
 
   return (
     <Container>
-      <Particle />
-      <hr></hr>
-      <hr></hr>
-      <h1 className="product-gallery-header">GALERÍA DE PRODUCTOS</h1>
+      <hr />
+      <hr />
+      <hr />
+      <h4 className="product-gallery-header">GALERÍA DE PRODUCTOS</h4>
+
       <InputGroup className="mb-3 search-bar-container">
         <FormControl
           placeholder="Buscar productos..."
@@ -77,20 +109,58 @@ function Product() {
         />
         <FormControl as="select" onChange={handleCategoryChange} value={selectedCategory}>
           <option value="">Todas las Categorías</option>
-          <option value="Categoría A">Vasos Tequileros</option>
-          <option value="Categoría B">Decoracion</option>
-          <option value="Categoría C">Servilletero</option>
-          <option value="Categoría D">Tecnologia</option>
-          <option value="Categoría E">Perfume</option>
+          <option value="Categoría A"> A-Vasos Tequileros</option>
+          <option value="Categoría B"> B-Decoracion</option>
+          <option value="Categoría C"> C-Servilletero</option>
+          <option value="Categoría D"> D-Tecnologia</option>
+          <option value="Categoría E"> E-Perfume</option>
         </FormControl>
-        <Button variant="outline-secondary">Buscar</Button>
+        <FormControl as="select" onChange={handleStarChange} value={selectedStar}>
+          <option value="">Todas las calificaciones</option>
+          <option value="5">5 Estrellas</option>
+          <option value="4">4 Estrellas</option>
+          <option value="3">3 Estrellas</option>
+          <option value="2">2 Estrellas</option>
+          <option value="1">1 Estrella</option>
+        </FormControl>
       </InputGroup>
+
+      <InputGroup>
+        <Row className="align-items-center">
+          <Col xs={12} sm={6} md={6} lg={6}>
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleDateChange}
+              dateFormat="dd/MM/yyyy"
+              placeholderText="Seleccionar fecha"
+              className="form-control"
+            />
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={6} className="mt-3 mt-sm-0 mt-md-0 mt-lg-0">
+            <Button variant="primary" onClick={handleReset} block>
+              Restablecer búsqueda
+            </Button>
+          </Col>
+        </Row>
+      </InputGroup>
+
       <Row>
         {filteredProducts.map(product => (
           <Col key={product.id} xs={12} md={4} lg={3} className="mb-4">
             <div className="product-item">
-              <img src={product.imageUrl} alt={product.title} className="img-fluid" />
-              <h5>{product.title}</h5>
+              <img src={product.imageUrl} alt={product.title} className="img-fluid"/>
+              <h4>{product.title}</h4>
+              {showDetails[product.id - 1] && (
+                <>
+                  <h6>{product.category}</h6>
+                  <h6>{new Date(product.date).toLocaleDateString()}</h6>
+                  {/* Otros detalles del producto */}
+                </>
+              )}
+              <Button variant="primary" onClick={() => handleShowDetails(product.id)}>
+                {showDetails[product.id - 1] ? 'Mostrar menos' : 'Mostrar más'}
+              </Button>
+              {/* Compartir en redes sociales */}
               <div className="footer-body">
                 <h6>¡Comparte en tus redes sociales!</h6>
                 <a href="https://www.instagram.com/delcidaziel/" style={{ color: "white" }} target="_blank" rel="noopener noreferrer">
@@ -107,13 +177,11 @@ function Product() {
                 name="product-rating" 
                 starCount={5}
                 value={product.rating}
-                onStarClick={(nextValue, prevValue) => onStarClick(nextValue, prevValue, product.id)}
+                onStarClick={(nextValue, _) => onStarClick(nextValue, _, product.id)}
               />
             </div>
           </Col>
         ))}
-
-
       </Row>
     </Container>
   );
